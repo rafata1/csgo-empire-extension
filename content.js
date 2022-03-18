def8413b28256fd7c77cd1562526ddadd576b45f
Dung CDA <dung.cda@teko.vn>
2022-03-18 17:35:43 +0700

    update

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 
 let IsBet = 0
+let countYellow = 0
+let countBlack = 0
+let countWhite = 0
+let IsIncreased = 0
+let previous = 0
+
 function filterContent() {
     let rollingTime = document.getElementsByClassName("text-2xl font-bold font-numeric")
     let t
@@ -12,10 +18,12 @@ function filterContent() {
             bet(2)
         }
     }
-
+    if (t && t != "0.00") {
+        IsIncreased = 0
+    } 
     if (t && t == "0.00") {
+        console.log("data: ", countYellow, countBlack, countWhite)
         IsBet = 0
-        IsEnterAmount = 0
         let betValues = document.getElementsByClassName("whitespace-nowrap font-numeric")
         if (betValues.length > 0) {
             console.log("betValues----------------")
@@ -24,12 +32,26 @@ function filterContent() {
                 if (bv.parentElement.innerHTML.includes('+')) {
                     if (i == 2) {
                         console.log("black win!!")
+                        if (IsIncreased == 0) {
+                            countBlack = countBlack + 1
+                            IsIncreased = 1
+                        }
                     }
                     if (i == 3) {
                         console.log("white win!!")
+                        if (IsIncreased == 0) {
+                            countWhite = countWhite + 1
+                            IsIncreased = 1
+                            console.log("Interval: ", countBlack + countYellow + countWhite - previous)
+                            previous = countBlack + countYellow + countWhite
+                        }
                     }
                     if (i == 4) {
                         console.log("yellow win!!")
+                        if (IsIncreased == 0) {
+                            countYellow = countYellow + 1
+                            IsIncreased = 1
+                        }
                     }
                 }
             }
