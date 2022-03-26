c347e5159f98d3afd26333aafa8dec3c63998ca4
dung.cda <dung.cda@teko.vn>
2022-03-26 15:16:35 +0700

    update

--- content.js
+++ content.js
@@ -3,60 +3,120 @@ let countYellow = 0
 let countBlack = 0
 let countWhite = 0
 let IsIncreased = 0
-let LostSteakYY = -1
-let LostSteakBB = -1
-let LostSteakYB = -1
-let LostSteakBY = -1
-let PreviousRes = ""
+let LostSteakRule1 = -1
+let LostSteakRule2 = -1
+let Mode
+let PreviousChoice
+let CurrentChoice
+let BetAmount
 
 function total() {
     return countBlack + countWhite + countYellow
 }
 
+function switchMode() {
+    if (Mode == 1 && LostSteakRule2 > 8) {
+        Mode = 2
+    } else
+        if (Mode == 2 && LostSteakRule1 > 8) {
+            Mode = 1
+        }
+}
+
+function resetBetAmount() {
+    BetAmount = 0.01
+    let clearBtn = document.getElementsByClassName("bet-input__control")[0]
+    clearBtn.click()
+    let add001Btn = document.getElementsByClassName("bet-input__control")[1]
+    add001Btn.click()
+}
+
+function doubleBetAmount() {
+    BetAmount *= 2
+    let doubleBtn = document.getElementsByClassName("bet-input__control")[7]
+    doubleBtn.click()
+}
+
+function afterResult(res) {
+    if (total() == 1) {
+        Mode = 1
+        if (res == "yellow") {
+            PreviousChoice = 1
+        } else
+        {
+            PreviousChoice = 0
+        }
+        return
+    }
+    if (res == "dice") {
+        doubleBetAmount()
+        LostSteakRule1++
+        LostSteakRule2++
+        return
+    }
+
+    if ((res == "black" && CurrentChoice == 0) || (res == "yellow" && CurrentChoice == 1)) {
+        if (Mode == 1) {
+            LostSteakRule1 = 0
+            LostSteakRule2++
+        } else {
+            LostSteakRule2 = 0
+            LostSteakRule1++
+        }
+        resetBetAmount()
+        switchMode()
+    } else {
+        if (Mode == 1) {
+            LostSteakRule1++
+            LostSteakRule2 = 0
+        } else {
+            LostSteakRule2++
+            LostSteakRule1 = 0
+        }
+        doubleBetAmount()
+    }
+}
+
 function bet() {
-    console.log("betted")
+    // 1 equal yellow, 0 equal black
+    if (Mode == 1) {
+        CurrentChoice = PreviousChoice
+    } else if (Mode == 2) {
+        CurrentChoice = 1 - PreviousChoice
+    }
+
+    let placeBetButtons = document.getElementsByClassName("bet-btn")
+    if (myChoice == 0) {
+        placeBetButtons[0].click()
+    }
+    if (myChoice == 1) {
+        placeBetButtons[2].click()
+    }
+    PreviousChoice = CurrentChoice
+    IsBet = 1
 }
 
-function calculateLostSteak(res) {
-    if (res == "yellow") {
-        if (PreviousRes == "yellow") {
-            LostSteakYY = 0
-            LostSteakBY += 1
-            LostSteakBB += 1
-            LostSteakYB += 1
-        }
-        if (PreviousRes == "black")
-    }
-    if (res == "black") {
-        if (LastY != -1) {
-            LostSteakYB = 0
-            LostSteakYY = total() - LastY
-        }
-        if (LastB != -1) {
-            LostSteakBB = 0
-            LostSteakBY = total() - LastB
-        }
-
-    }
-    PreviousRes = res
-    if (res == "yellow") LastY = total()
-    if (res == "black") LastB = total()
-    console.log("-------------------")
-    console.log("BY: ", LostSteakBY)
-    console.log("YY: ", LostSteakYY)
-    console.log("YB: ", LostSteakYB)
-    console.log("BB: ", LostSteakBB)
-    console.log("-------------------")
+function ChoiceToString(c) {
+    if (c == 0) return "black"
+    if (c == 1) return "yellow"
 }
 
+function logging(res) {
+    if (total() == 1) {
+        console.log("SET MODE 1 WITH CHOICE", PreviousChoice)
+        return
+    }
+    console.log("MODE:", Mode, "CHOICE:", ChoiceToString(CurrentChoice), "RESULT:", res, "AMOUNT:", BetAmount, "LS1:", LostSteakRule1, "LS2:", LostSteakRule2)
+    return
+}
 
 function filterContent() {
     let rollingTime = document.getElementsByClassName("text-2xl font-bold font-numeric")
     let t
     if (rollingTime.length > 0) {
         t = rollingTime[0].innerHTML
-        if (1 < parseFloat(t) && parseFloat(t) < 5 && IsBet == 0) {
-            IsBet = 1
+        // bet at last 5 seconds, bet only have the first result
+        if (1 < parseFloat(t) && parseFloat(t) < 5 && IsBet == 0 && total() > 0) {
             bet()
         }
     }
@@ -66,7 +126,6 @@ function filterContent() {
     }
 
     if (t && t == "0.00") {
-        console.log("data: ", countBlack, countWhite, countYellow)
         IsBet = 0
         let betValues = document.getElementsByClassName("whitespace-nowrap font-numeric")
         if (betValues.length > 0) {
@@ -76,19 +135,21 @@ function filterContent() {
                     if (IsIncreased == 0) {
                         let res = ""
                         IsIncreased = 1
+                        console.log("data: ", countBlack, countWhite, countYellow)
                         if (i == 2) {
                             res = "black"
                             countBlack = countBlack + 1
                         }
                         if (i == 3) {
-                            res = "white"
+                            res = "dice"
                             countWhite = countWhite + 1
                         }
                         if (i == 4) {
                             res = "yellow"
                             countYellow = countYellow + 1
                         }
-                        calculateLostSteak(res)
+                        afterResult(res)
+                        logging(res)
                     }
                 }
             }
