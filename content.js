4bc5cec7f384f65e0231324fc0a014670c380962
dung.cda <dung.cda@teko.vn>
2022-03-27 23:50:03 +0700

    update

--- content.js
+++ content.js
@@ -11,9 +11,12 @@ let CurrentChoice
 let PreviousRes
 let BetAmount = 0.01
 let PreviousBetAmount = 0.01
-let VirMoney = 100
+let VirMoney
 let IsStarted = false
 let Eated = false
+let SetUi = false
+let MaxLostStreak = 7
+let configAmount = 1
 
 function total() {
     return countBlack + countWhite + countYellow
@@ -21,11 +24,15 @@ function total() {
 
 function switchMode(res) {
     if ((res == "black" && CurrentChoice == 0) || (res == "yellow" && CurrentChoice == 1)) {
-        if (Mode == 1 && LostSteakRule2 > 7) {
+        if (inputX.value > 0) {
+            MaxLostStreak = inputX.value
+            console.log("set max loststreak to ", MaxLostStreak)
+        }
+        if (Mode == 1 && LostSteakRule2 > MaxLostStreak) {
             Mode = 2
             console.log("CHANGE TO MODE: ", Mode)
         }
-        else if (Mode == 2 && LostSteakRule1 > 7) {
+        else if (Mode == 2 && LostSteakRule1 > MaxLostStreak) {
             Mode = 1
             console.log("CHANGE TO MODE: ", Mode)
         }
@@ -33,22 +40,37 @@ function switchMode(res) {
 }
 
 function resetBetAmount() {
-    BetAmount = 0.01
+    // BetAmount = 0.01
     let clearBtn = document.getElementsByClassName("bet-input__control")[0]
     clearBtn.click()
-    let add001Btn = document.getElementsByClassName("bet-input__control")[1]
-    add001Btn.click()
+    if (configAmount == 1) {
+        let add001Btn = document.getElementsByClassName("bet-input__control")[1]
+        add001Btn.click()
+        add001Btn.click()
+        add001Btn.click()
+    }
+    if (configAmount == 2) {
+        let add001Btn = document.getElementsByClassName("bet-input__control")[1]
+        add001Btn.click()
+        add001Btn.click()
+        add001Btn.click()
+        add001Btn.click()
+        add001Btn.click()
+    }
+    if (configAmount == 3) {
+        let add01Btn = document.getElementsByClassName("bet-input__control")[2]
+        add01Btn.click()
+    }
+    PreviousBetAmount = document.getElementsByClassName("bg-transparent w-full h-full relative z-10")[0].value
 }
-
 function doubleBetAmount() {
-    BetAmount *= 2.0
+    // BetAmount *= 2.0
     let doubleBtn = document.getElementsByClassName("bet-input__control")[7]
     doubleBtn.click()
 }
 
 function afterResult(res) {
     //storing previous betamount
-    PreviousBetAmount = BetAmount
     PreviousRes = res
     if (total() == 1) {
         Mode = 1
@@ -116,7 +138,7 @@ function bet() {
     }
     PreviousChoice = CurrentChoice
     IsBet = 1
-    VirMoney -= BetAmount
+    // VirMoney -= BetAmount
     Eated = false
 }
 
@@ -130,11 +152,54 @@ function logging(res) {
         console.log("SET MODE 1 WITH CHOICE", ChoiceToString(PreviousChoice))
         return
     }
-    console.log("MODE:", Mode, "CHOICE:", ChoiceToString(CurrentChoice), "RESULT:", res, "AMOUNT:", PreviousBetAmount, "LS1:", LostSteakRule1, "LS2:", LostSteakRule2, "MONEY:", VirMoney.toFixed(2))
+    VirMoney = document.getElementsByClassName("whitespace-nowrap font-numeric")[0].innerText
+    console.log("MODE:", Mode, "CHOICE:", ChoiceToString(CurrentChoice), "RESULT:", res, "AMOUNT:", PreviousBetAmount, "LS1:", LostSteakRule1, "LS2:", LostSteakRule2, "MONEY:", VirMoney)
     return
 }
 
+function handleStartClick() {
+    if (startBtn.innerText == "start") {
+        startBtn.innerText = "stop"
+    } else {
+        startBtn.innerText = "start"
+    }
+    IsStarted = !IsStarted
+}
+
+const startBtn = document.createElement("Button")
+startBtn.innerText = "start"
+startBtn.onclick = handleStartClick
+
+const inputX = document.createElement("input")
+inputX.placeholder = "x....."
+inputX.setAttribute("type", "number")
+
+const a001 = document.createElement("button")
+a001.innerText = "0.01"
+a001.onclick = () => { configAmount = 1 }
+
+const a005 = document.createElement("button")
+a005.innerText = "0.05"
+a005.onclick = () => { configAmount = 2 }
+
+const a01 = document.createElement("button")
+a01.innerText = "0.1"
+a01.onclick = () => { configAmount = 3 }
+
+function SetupUI() {
+    SetUi = true
+    let container = document.getElementsByClassName("layout")[0]
+    container.appendChild(startBtn)
+    container.appendChild(a001)
+    container.appendChild(a005)
+    container.appendChild(a01)
+    container.appendChild(inputX)
+}
+
 function filterContent() {
+    if (!SetUi) {
+        SetupUI()
+    }
     let rollingTime = document.getElementsByClassName("text-2xl font-bold font-numeric")
     let t
     if (rollingTime.length > 0) {
@@ -186,72 +251,4 @@ function filterContent() {
 }
 
 
-function handleStartClick() {
-    if (startBtn.innerText == "start") {
-        startBtn.innerText = "stop"
-    } else {
-        startBtn.innerText = "start"
-    }
-    IsStarted = !IsStarted
-}
-
-const startBtn = document.createElement("Button")
-startBtn.innerText = "start"
-startBtn.onclick = handleStartClick
-
-document.getElementsByClassName("layout")[0].appendChild(startBtn)
-
 setInterval(filterContent, 1500)
-// function runVirtual(ress) {
-//     for (let i = 0; i < ress.length; i++) {
-//         resetBetAmount()
-//         if (total() > 0) bet();
-//         res = ress[i]
-//         if (res == "yellow") {
-//             countYellow ++
-//         }
-//         if (res == "black") {
-//             countBlack ++
-//         }
-//         if (res == "dice") {
-//             countWhite ++
-//         }
-//         afterResult(res)
-//         logging(res)
-//         switchMode(res)
-//     }
-// }
-
-// runVirtual([
-//     "dice",
-//     "yellow",
-//     "black",
-//     "yellow",
-//     "black",
-//     "yellow",
-//     "black",
-//     "yellow",
-//     "yellow",
-//     "dice",
-//     "black",
-//     "black",
-//     "yellow",
-//     "black",
-//     "black",
-//     "black",
-//     "black",
-//     "black",
-//     "black",
-//     "black",
-//     "black",
-//     "yellow",
-//     "dice",
-//     "yellow",
-//     "black",
-//     "dice",
-//     "yellow",
-//     "black",
-//     "dice",
-//     "yellow",
-//     "black"
-// ])
